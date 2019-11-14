741948b5d4450d2fb964fcc328ac4efb70d2cc8e
金霜 <892546073@qq.com>
2019-11-14 16:38:30 +0800

    第五次增加了头像上传预览功能

--- public/assets/js/users.js
+++ public/assets/js/users.js
@@ -18,3 +18,25 @@ $("#userForm").on('submit', function () {
     //阻止表单的默认提交行为 
     return false;
 })
+//当用户选择文件的时候
+$('#modifyBox').on('change','#avatar', function () {  
+    //用户选择到的文件this.files[0]
+    var formData = new FormData();
+    formData.append('avatar', this.files[0]);
+    $.ajax({
+        type:'post',
+        url:'/upload',
+        data:formData,
+        //告诉$.ajax方法不要解析请求参数
+        processData:false,
+        //告诉$.ajax方法不要设置请求参数的类型
+        contentType:false,
+        success: function (response) { 
+            console.log(response); 
+            //实现头像预览功能
+            $('#preview').attr('src',response[0].avatar);
+            //设置隐藏域的值，到时候点击提交时需要发送给服务器
+            $('#hiddenAvatar').val(response[0].avatar);
+        }
+    })
+})
