4ff9a3aa95d01ab989ce785e29cf432dd1f956bf
金霜 <892546073@qq.com>
2019-11-14 19:47:38 +0800

    第7次添加了用户信息编辑功能

--- public/assets/js/users.js
+++ public/assets/js/users.js
@@ -1,7 +1,7 @@
 //当表单发表提交行为的时候
 $("#userForm").on('submit', function () {
     //获取到用户在表单中输入的内容并将内容格式化成参数字符串
-    var formData = $(this).serialize(); 
+    let formData = $(this).serialize(); 
     //向服务器端发送添加用户的请求
     $.ajax({
         type:'post',
@@ -21,7 +21,7 @@ $("#userForm").on('submit', function () {
 //当用户选择文件的时候
 $('#modifyBox').on('change','#avatar', function () {  
     //用户选择到的文件this.files[0]
-    var formData = new FormData();
+    let formData = new FormData();
     formData.append('avatar', this.files[0]);
     $.ajax({
         type:'post',
@@ -32,7 +32,7 @@ $('#modifyBox').on('change','#avatar', function () {
         //告诉$.ajax方法不要设置请求参数的类型
         contentType:false,
         success: function (response) { 
-            console.log(response); 
+            // console.log(response); 
             //实现头像预览功能
             $('#preview').attr('src',response[0].avatar);
             //设置隐藏域的值，到时候点击提交时需要发送给服务器
@@ -45,10 +45,40 @@ $.ajax({
     type:'get',
     url:'/users',
     success: function (response) { 
-        console.log(response); 
+        // console.log(response); 
         //使用模版引擎将数据和html字符串进行拼接
-        var html = template('userTpl',{data: response});
+        let html = template('userTpl',{data: response});
         //将拼接好的字符串显示在页面中
         $('#userBox').html(html);
     }
 });
+//通过事件委托的方式为编辑按钮添加点击事件
+$('#userBox').on('click','.edit', function () {  
+    //获取被点击用户的id值
+    let id = $(this).attr('data-id');
+    //根据id获取用户的详细信息
+    $.ajax({
+        type:'get',
+        // url:'/users/' + id,下面👇是ES6的新语法
+        url:`/users/${id}`,
+        success: function(response) {  
+            console.log(response);
+            let html = template('modifyTpl',response);
+            $('#modifyBox').html(html);
+        }
+    })
+})
+//
+$('#modifyBox').on('submit', '#modifyForm', function () {  
+    //
+    let id = $(this).attr('data-id');
+    let formData = $(this).serialize();
+    $.ajax({
+        type:'put',
+        url:`/users/${id}`,
+        data:formData,
+        success: function (response) {  
+            location.reload();
+        }
+    })
+})
